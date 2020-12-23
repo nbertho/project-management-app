bc5b5bc5ddc85b5b2860b14f3d764d7804bde0c8
Nicolas <n.bertho.web@gmail.com>
2020-12-23 21:38:38 +0100

    Removed unused route from projects.js

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -10,17 +10,6 @@ router.use((req, res, next) => {
 });
 
 
-router.get('/', async (req, res) => {
-  try {
-    let projectIndex = await db.promise().query(`SELECT * FROM project`);
-    res.status(200).send(projectIndex[0]);
-  } 
-  catch (error) {
-    res.status(403).send(error);
-  }
-});
-
-
 // CREATE PROJECT
 router.put('/create', async (req, res) => {
   const schema = Joi.object({
