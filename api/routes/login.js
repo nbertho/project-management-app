69842bdda784c65c4b1fc7bf2b8d835005e77aeb
Nicolas Bertho <n.bertho.web@gmail.com>
2020-12-25 19:08:32 +0100

    Created token with project id on loggin

--- api/routes/login.js
+++ api/routes/login.js
@@ -28,12 +28,21 @@ router.post('/login', async (req, res, next) => {
   else {
     try {
       db.query('SELECT * from USERS WHERE email LIKE ?;', req.body.mail, function(err, result, field) {
-        if(result.length ==1) {
-          dbMail = result[0].email;
-          dbPwd = result[0].password;
+        if(result.length == 1) {
+          const dbMail = result[0].email;
+          const dbPwd = result[0].password;
+          const dbId = result[0].id;
           bcrypt.compare(req.body.pwd, dbPwd).then(function(result) {
             if (result == true) {
-              res.send(200).send('PASSWORD MATCH');
+              db.query('SELECT project_id from users_has_project WHERE users_id =  ?;', dbId, function(err, projectResult, field) {
+                let idArray = "";
+                projectResult.forEach(id => {
+                  idArray = idArray + id.project_id + ','
+                });
+                idArray = idArray.substring(0, idArray.length - 1);
+                req.session.project_list_id = idArray;
+                res.status(400).json({success: 'Logged in'});
+              });
             }
             else {
               res.status(403).json({error: 'Wrong credentials'});
